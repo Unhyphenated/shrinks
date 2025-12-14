9347556557c60c0c9d989ac769698c2733bd5ca0
Julian Jong <julian.jong74@gmail.com>
2025-12-14 22:02:54 +0800

    fix: make json utils public

--- internal/util/json.go
+++ internal/util/json.go
@@ -6,19 +6,18 @@ import (
 	"net/http"
 )
 
-func writeJSON(w http.ResponseWriter, status int, v interface{}) {
+func WriteJSON(w http.ResponseWriter, status int, v interface{}) {
 	w.Header().Set("Content-Type", "application/json")
-    
+		
 	w.WriteHeader(status)
 
 	if err := json.NewEncoder(w).Encode(v); err != nil {
-		// Log the failure, but since we already wrote headers, we can't send a clean error response.
-		log.Printf("Error writing JSON response: %v", err)
+	log.Printf("Error writing JSON response: %v", err)
 	}
 }
 
-func writeError(w http.ResponseWriter, status int, message string) {
+func WriteError(w http.ResponseWriter, status int, message string) {
     errorPayload := map[string]string{"error": message}
-	
-	writeJSON(w, status, errorPayload)
+
+	WriteJSON(w, status, errorPayload)
 }
