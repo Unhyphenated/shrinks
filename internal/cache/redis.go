13cf626a8d98dff68396822ee3d52629b64727ba
Julian Jong <julian.jong74@gmail.com>
2025-12-28 00:09:30 +0800

    feat: add get, set and close methods for redis client

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -1,4 +1,4 @@
-package main
+package cache
 
 import (
 	"context"
@@ -8,24 +8,47 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-type Cache interface {
-	Get(ctx context.Context, key string) (string, error)
-	Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error
-	Close()
+type RedisClient struct {
+	Client *redis.Client
 }
 
-func NewRedisClient(redisURL string) (*redis.Client, error) {
-	rdb := redis.NewClient(&redis.Options{
-		Addr: redisURL,
-		Password: "",
-		DB: 0,
-	})
+func NewRedisClient(redisURL string) (*RedisClient, error) {
+	opts, err := redis.ParseURL(redisURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse Redis URL: %w", err)
+	}
+	client := redis.NewClient(opts)
 
 	ctx := context.Background()
-	_, err := rdb.Ping(ctx).Result()
+	_, err = client.Ping(ctx).Result()
 	if err != nil {
 		return nil, fmt.Errorf("failed to ping Redis: %w", err)
 	}
 
-	return rdb, nil
+	fmt.Println("Successfully initialized Redis Client!")
+
+	return &RedisClient{Client: client}, nil
+}
+
+func (c *RedisClient) Get(ctx context.Context, key string) (string, error) {
+	val, err := c.Client.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return "", fmt.Errorf("key not found in Redis")
+	} else if err != nil {
+		return "", fmt.Errorf("failed to get value from Redis: %w", err)
+	}
+
+	return val, nil
+}
+
+func (c *RedisClient) Set(ctx context.Context, key string, val string, expiration time.Duration) error {
+	err := c.Client.Set(ctx, key, val, expiration).Err()
+	if err != nil {
+		return fmt.Errorf("failed to set value in Redis: %w", err)
+	}
+	return nil
+}
+
+func (c *RedisClient) Close() {
+	c.Client.Close()
 }
