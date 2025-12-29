5dd4fc4362bef8b4f43b95c8a61a319b2f3ea354
Julian Jong <julian.jong74@gmail.com>
2025-12-29 23:00:12 +0800

    fix: serialise Link struct when setting k-v pair in Redis

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -4,15 +4,17 @@ import (
 	"context"
 	"fmt"
 	"time"
+	"encoding/json"
 
+	"github.com/Unhyphenated/shrinks-backend/internal/model"
 	"github.com/redis/go-redis/v9"
 )
 
-type RedisClient struct {
+type Cache struct {
 	Client *redis.Client
 }
 
-func NewRedisClient(redisURL string) (*RedisClient, error) {
+func NewRedisCache(redisURL string) (*Cache, error) {
 	opts, err := redis.ParseURL(redisURL)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse Redis URL: %w", err)
@@ -27,28 +29,38 @@ func NewRedisClient(redisURL string) (*RedisClient, error) {
 
 	fmt.Println("Successfully initialized Redis Client!")
 
-	return &RedisClient{Client: client}, nil
+	return &Cache{Client: client}, nil
 }
 
-func (c *RedisClient) Get(ctx context.Context, key string) (string, error) {
+func (c *Cache) Get(ctx context.Context, key string) (*model.Link, error) {
 	val, err := c.Client.Get(ctx, key).Result()
 	if err == redis.Nil {
-		return "", fmt.Errorf("key not found in Redis")
+		return &model.Link{}, nil
 	} else if err != nil {
-		return "", fmt.Errorf("failed to get value from Redis: %w", err)
+		return nil, fmt.Errorf("failed to get value from Redis: %w", err)
 	}
 
-	return val, nil
+	return &model.Link{
+		LongURL: val,
+		ShortURL: key,
+		Clicks: 0,
+		CreatedAt: time.Now(),
+	}, nil
 }
 
-func (c *RedisClient) Set(ctx context.Context, key string, val string, expiration time.Duration) error {
-	err := c.Client.Set(ctx, key, val, expiration).Err()
+func (c *Cache) Set(ctx context.Context, key string, val *model.Link, expiration time.Duration) error {
+	jsonData, err := json.Marshal(val)
+	if err != nil {
+		return fmt.Errorf("Failed to marshal link to JSON: %w", err)
+	}
+
+	err = c.Client.Set(ctx, key, jsonData, expiration).Err()
 	if err != nil {
 		return fmt.Errorf("failed to set value in Redis: %w", err)
 	}
 	return nil
 }
 
-func (c *RedisClient) Close() {
+func (c *Cache) Close() {
 	c.Client.Close()
 }
