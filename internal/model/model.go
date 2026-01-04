65989abb8daa82d2592383d81589c638d78c01f9
Julian Jong <julian.jong74@gmail.com>
2026-01-04 17:55:19 +0800

    feat: add user model

--- internal/model/model.go
+++ internal/model/model.go
@@ -2,12 +2,18 @@ package model
 
 import "time"
 
+type User struct {
+	ID uint64 `db:"id"`
+	Email string `db:"email"`
+	PasswordHash string `db:"password_hash"`
+	CreatedAt time.Time `db:"created_at"`
+}
+
 type Link struct {
 	ID uint64 `db:"id"`
-	ShortURL string `db:"short_url"`
+	ShortCode string `db:"short_code"`
 	LongURL string `db:"long_url"`
 	CreatedAt time.Time `db:"created_at"`
-	Clicks uint64 `db:"clicks"`
 }
 
 type CreateLinkRequest struct {
