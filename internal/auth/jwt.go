cd083235bcdd3a7c771069b616d8483dc704f62f
Julian Jong <julian.jong74@gmail.com>
2026-01-08 00:40:51 +0800

    feat: write generate token function

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,6 +1,10 @@
 package auth
 
 import (
+	"os"
+	"errors"
+	"time"
+
 	"github.com/golang-jwt/jwt/v5"
 )
 
@@ -10,3 +14,23 @@ type Claims struct {
     jwt.RegisteredClaims  // Built-in struct with exp, iat, iss, etc.
 }
 
+func GenerateToken(userID int, email string) (string, error) {
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		return "", errors.New("JWT_SECRET is not set")
+	}
+
+	claims := &Claims{
+		UserID: userID,
+		Email: email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Issuer: "shrinks",
+			Subject: email,
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
