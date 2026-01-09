8b606f238c1ccee77cbe0d0dbd56e2cf8cbeda11
Julian Jong <julian.jong74@gmail.com>
2026-01-10 00:25:44 +0800

    feat: add extract token

---
+++ internal/auth/middleware.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	"strings"
+	"errors"
+	"net/http"
+
+)
+
+func extractToken(r *http.Request) (string, error) {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		return "", errors.New("missing Authorization header")
+	}
+
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		return "", errors.New("invalid Authorization header")
+	}
+
+	token := parts[1]
+
+	return token, nil
+}
