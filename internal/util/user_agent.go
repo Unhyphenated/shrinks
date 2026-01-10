c254018d41e38748309e8a8ca3dc3970556463ca
Julian Jong <julian.jong74@gmail.com>
2026-01-10 21:56:17 +0800

    feat: clean up user agent utils

--- internal/util/user_agent.go
+++ internal/util/user_agent.go
@@ -3,45 +3,47 @@ package util
 import (
 	"fmt"
 	"net"
-	"net/http"
 	"strings"
 
 	"github.com/mileusna/useragent"
 )
 
-func ParseUserAgent(uaString string) (browser, os, deviceType string) {
-    ua := useragent.Parse(uaString)
-
-    browser = fmt.Sprintf("%s %s", ua.Name, ua.Version)
-    os = ua.OS
-
-    if ua.Mobile {
-        deviceType = "Mobile"
-    } else if ua.Tablet {
-        deviceType = "Tablet"
-    } else if ua.Desktop {
-        deviceType = "Desktop"
-    } else if ua.Bot {
-        deviceType = "Bot"
-    } else {
-        deviceType = "Unknown"
-    }
-    
-    // Clean up strings
-    browser = strings.TrimSpace(browser)
-    os = strings.TrimSpace(os)
-    deviceType = strings.TrimSpace(deviceType)
-
-    return browser, os, deviceType
+type UserAgentInfo struct {
+    Browser    string
+    OS         string
+    DeviceType string
 }
 
-func GetIP(r *http.Request) string {
+func ParseUserAgent(uaString string) UserAgentInfo {
+    ua := useragent.Parse(uaString)
+    
+    // logic remains the same, just return the struct
+    return UserAgentInfo{
+        Browser:    fmt.Sprintf("%s %s", ua.Name, ua.Version),
+        OS:         ua.OS,
+        DeviceType: determineDevice(ua), // break logic into small private funcs
+    }
+}
+
+func determineDevice(ua useragent.UserAgent) string {
+    if ua.Mobile {
+        return "Mobile"
+    } else if ua.Tablet {
+        return "Tablet"
+    } else if ua.Desktop {
+        return "Desktop"
+    } else if ua.Bot {
+        return "Bot"
+    }
+    return "Unknown"
+}
+
+func GetIP(ip string, remoteAddr string) string {
     // Check if we are behind a proxy (like Nginx, Docker, or Cloudflare)
-    ip := r.Header.Get("X-Forwarded-For")
     if ip == "" {
         // No proxy, get it directly
         // net.SplitHostPort removes the port number (e.g., "192.168.1.1:54321" -> "192.168.1.1")
-        ip, _, _ = net.SplitHostPort(r.RemoteAddr)
+        ip, _, _ = net.SplitHostPort(remoteAddr)
         return ip
     }
     
