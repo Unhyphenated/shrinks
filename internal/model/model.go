e25f8d71a44c55859db069afb551532c09d0c454
Julian Jong <julian.jong74@gmail.com>
2026-01-12 22:49:22 +0800

    feat: add analytics summary models

--- internal/model/model.go
+++ internal/model/model.go
@@ -4,14 +4,14 @@ import "time"
 
 // User Models
 type User struct {
-	ID uint64 `db:"id"`
-	Email string `db:"email"`
-	PasswordHash string `db:"password_hash" json:"-"`
-	CreatedAt time.Time `db:"created_at"`
+	ID           uint64    `db:"id"`
+	Email        string    `db:"email"`
+	PasswordHash string    `db:"password_hash" json:"-"`
+	CreatedAt    time.Time `db:"created_at"`
 }
 
 type RegisterRequest struct {
-	Email string `json:"email"`
+	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
@@ -20,20 +20,20 @@ type RegisterResponse struct {
 }
 
 type LoginRequest struct {
-	Email string `json:"email"`
+	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
 type AuthResponse struct {
-	AccessToken string `json:"access_token"`
+	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
-	User User `json:"user"`
+	User         User   `json:"user"`
 }
 
 type RefreshToken struct {
-	ID uint64 `db:"id"`
-	UserID uint64 `json:"user_id"`
-	TokenHash string `json:"token_hash"`
+	ID        uint64    `db:"id"`
+	UserID    uint64    `json:"user_id"`
+	TokenHash string    `json:"token_hash"`
 	ExpiresAt time.Time `json:"expires_at"`
 	CreatedAt time.Time `db:"created_at"`
 }
@@ -48,31 +48,69 @@ type RefreshTokenResponse struct {
 
 // Link Models
 type Link struct {
-	ID uint64 `db:"id"`
-	UserID *uint64 `db:"user_id"`
-	ShortCode string `db:"short_code"`
-	LongURL string `db:"long_url"`
+	ID        uint64    `db:"id"`
+	UserID    *uint64   `db:"user_id"`
+	ShortCode string    `db:"short_code"`
+	LongURL   string    `db:"long_url"`
 	CreatedAt time.Time `db:"created_at"`
 }
 
 type CreateLinkRequest struct {
-	URL string `json:"url"`
+	URL        string `json:"url"`
 	CustomCode string `json:"custom_code"`
 }
 
 type CreateLinkResponse struct {
 	ShortCode string `json:"short_code"`
-	LongURL string `json:"long_url"`
+	LongURL   string `json:"long_url"`
 }
 
 // Analytics Models
 type AnalyticsEvent struct {
-	ID uint64 `db:"id"`
-	LinkID uint64 `db:"link_id"`
-	IPAddress string `db:"ip_address"`
-	UserAgent string `db:"user_agent"`
-	DeviceType string `db:"device_type"`
-	OS string `db:"os"`
-	Browser string `db:"browser"`
-	ClickedAt time.Time `db:"clicked_at"`
+	ID         uint64    `db:"id"`
+	LinkID     uint64    `db:"link_id"`
+	IPAddress  string    `db:"ip_address"`
+	UserAgent  string    `db:"user_agent"`
+	DeviceType string    `db:"device_type"`
+	OS         string    `db:"os"`
+	Browser    string    `db:"browser"`
+	ClickedAt  time.Time `db:"clicked_at"`
 }
+
+type AnalyticsSummary struct {
+	LinkID uint64 `db:"link_id"`
+	ShortCode string `db:"short_code"`
+	Period string `db:"period"`
+	TotalClicks int `db:"total_clicks"`
+	UniqueVisitors int `db:"unique_visitors"`
+	ClicksByDate []ClicksByDate `db:"clicks_by_date"`
+	ClicksByDevice []ClicksByDevice `db:"clicks_by_device"`
+	ClicksByBrowser []ClicksByBrowser `db:"clicks_by_browser"`
+	ClicksByOS []ClicksByOS `db:"clicks_by_os"`
+}
+
+type ClicksByDate struct {
+	Date time.Time `db:"date"`
+	Clicks int `db:"clicks"`
+}
+
+type ClicksByDevice struct {
+	Device string `db:"device"`
+	Clicks int `db:"clicks"`
+}
+
+type ClicksByBrowser struct {
+	Browser string `db:"browser"`
+	Clicks int `db:"clicks"`
+}
+
+type ClicksByOS struct {
+	OS string `db:"os"`
+	Clicks int `db:"clicks"`
+}
+
+// LinkWithStats - link with basic stats for list view
+// TODO: add fields:
+//   - ID, ShortCode, LongURL, CreatedAt
+//   - TotalClicks (from analytics aggregation)
+//   - UserID
